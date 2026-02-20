bf6fb91ec134000dc5b16768dd7310c385b0b7fb
Santiagopro2002 <cristianaguilac08@gmail.com>
2026-02-20 11:42:14 -0500

    subiuedno sping

---
+++ src/main/java/com/utc/tevcol/TevcolApplication.java
@@ -0,0 +1,13 @@
+package com.utc.tevcol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TevcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TevcolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utc/tevcol/TevcolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.tevcol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TevcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
